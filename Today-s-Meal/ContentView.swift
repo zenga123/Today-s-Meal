f83b2947b63db1f3cb168cd6f7c899006c4376c7
zenga123 <zenga85@naver.com>
2025-04-27 23:16:59 +0900

    Initial Commit

---
+++ Today-s-Meal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Today-s-Meal
+//
+//  Created by musung on 2025/04/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Today-s-Meal/Today_s_MealApp.swift
@@ -0,0 +1,17 @@
+//
+//  Today_s_MealApp.swift
+//  Today-s-Meal
+//
+//  Created by musung on 2025/04/27.
+//
+
+import SwiftUI
+
+@main
+struct Today_s_MealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
